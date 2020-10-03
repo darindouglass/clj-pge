803456eb81a9e7166a65e94d5f05dcd69b7bf3d4
Darin Douglass <ddouglass@barracuda.com>
2020-10-02 21:03:01 -0400

    Add initial project setup

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject douglass/clj-psql "0.1.0"
+  :description "An Clojure implementation of the oclPixelGameEngine"
+;;  :license "EPL "
+  :url "https://github.com/DarinDouglass/clj-pge"
+  :dependencies [[org.clojure/clojure "1.10.1"]])
